c03caed178d57a01054bdb6ec9e98ac2e5420acd
alfianguide <alfianguide@gmail.com>
2020-06-22 13:59:17 +0700

    first init

---
+++ apache.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+
+services.httpd.enable = true;
+services.httpd.adminAddr = "alfianguide@gmail.com";
+services.httpd.extraModules = [
+ {name = "php7"; path = "${pkgs.php}/modules/libphp7.so"; }
+];
+services.httpd.enablePHP = true;
+services.httpd.user = "alfianguide";
+services.httpd.group = "users";
+services.httpd.virtualHosts =
+    let
+      makeVirtualHost = { root, host }:
+        { documentRoot = root;
+          adminAddr = "admin@example.com";
+          forceSSL = true;
+	  sslServerCert = "/home/alfianguide/ssl/${host}.crt";
+	  sslServerKey = "/home/alfianguide/ssl/${host}.key";
+	  extraConfig = ''
+	  <Directory "${root}">
+          	 Options Indexes FollowSymLinks
+ 		 AllowOverride All
+		 Require all granted
+		 DirectoryIndex index.php index.html
+	  </Directory>
+        '';
+        };
+    in
+      { "siapmii.com" = (makeVirtualHost { root = "/home/alfianguide/prj/siapmii"; host = "localhost"; });
+        "devufi.io" = (makeVirtualHost { root = "/home/alfianguide/prj/ufiacademy"; host = "devufi"; });
+      };
+
+}
---
+++ services.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+   virtualisation.docker.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+}
