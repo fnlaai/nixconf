ecf524d72b326d81bd76cd1a4bded972368a6501
alfianguide <alfianguide@gmail.com>
2020-10-12 02:34:35 +0700

    fixio lagga

--- networking.nix
+++ networking.nix
@@ -8,9 +8,9 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp5s0f1.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
+#  networking.useDHCP = false;
+#  networking.interfaces.enp4s0.useDHCP = true;
+#  networking.interfaces.wlp6s0f3u3.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
