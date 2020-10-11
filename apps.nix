7772b38b8ad41d210b136a6dfbad67e9675a265d
alfianguide <alfianguide@gmail.com>
2020-10-12 02:47:50 +0700

    expelliarmus users.nix

--- apps.nix
+++ apps.nix
@@ -1,12 +1,23 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+     pinentryFlavor = "tty";
+   };
+
+  programs.fish.enable = true;
+  
   virtualisation.docker.enable = true;
   programs.adb.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #   wget emacs vscodium firefox fish spotify phpstorm
     xclip
   ];
 
--- users.nix
+++ users.nix
@@ -1,21 +1,10 @@
 { config, pkgs, ... }:
 
 {
-   nixpkgs.config.allowUnfree = true;
-
-   programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-     pinentryFlavor = "tty";
-   };
-
-  programs.fish.enable = true;
-  
   users.users.alfianguide = {
      isNormalUser = true;
      shell = pkgs.fish;
-     extraGroups = [ "wheel" "docker" "adbusers"]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "wheel" "docker" "adbusers"];
   };
 
   nix.trustedUsers = [ "root" "alfianguide" ];
